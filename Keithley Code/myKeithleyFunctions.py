62c6637e448762bc0826f08fa33d46707304ab35
Eli Wolf <eli.j.wolf@gmail.com>
2020-04-17 17:20:14 -0600

    Bug Fixes
    - fixed current limit default
    - removed eval functions

--- Keithley Code/myKeithleyFunctions.py
+++ Keithley Code/myKeithleyFunctions.py
@@ -9,7 +9,7 @@ from PyQt5 import QtTest
 - determine Digital Output configuration required to open and close the shutter. The functions currently work, but the output configuration is a guess.
 '''
 ###############
-def connectToKeithley(keithleyAddress='GPIB0::22::INSTR',rm='pyvisa.ResourceManager()'):
+def connectToKeithley(keithleyAddress='GPIB0::22::INSTR'):
 	'''
 	This creates a Keithley object with which to interact with.
 	Attempt to connect to a Keithley, then send an ID query to confirm connection. If this fails, send an error message to the terminal and terminate the program.
@@ -53,10 +53,11 @@ def connectToKeithley(keithleyAddress='GPIB0::22::INSTR',rm='pyvisa.ResourceMana
 		return keithleyAddress
 
 	try:
-# 		rm = pyvisa.ResourceManager()
+		global rm
+		rm = pyvisa.ResourceManager()
 		# print(rm.list_resources())
 		print ('Attempting to connect to keithley.')
-		keithleyObject = eval(rm).open_resource(keithleyAddress)
+		keithleyObject = rm.open_resource(keithleyAddress)
 		print (keithleyObject.query('*IDN?'))
 		# keithleyObject.timeout = 100000
 		keithleyObject.write('*RST')
@@ -67,7 +68,7 @@ def connectToKeithley(keithleyAddress='GPIB0::22::INSTR',rm='pyvisa.ResourceMana
 		sys.exit()
 	return keithleyObject
 
-def prepareVoltage(keithleyObject, NPLC=1):
+def prepareVoltage(keithleyObject, NPLC=1, voltlimit = 10):
 	'''
 	Prepares the Keithley to measure voltage.
 	NPLC Range [0.01,10]
@@ -78,7 +79,7 @@ def prepareVoltage(keithleyObject, NPLC=1):
 	keithleyObject.write('SOUR:CURR:MODE FIXED')
 	keithleyObject.write('SOUR:CURR:RANG:AUTO ON')
 	keithleyObject.write('SENS:FUNC "VOLT"')
-	keithleyObject.write('SENS:VOLT:PROT 10')
+	keithleyObject.write('SENS:VOLT:PROT {:.3f}}'.format(voltlimit))
 	keithleyObject.write('SENS:VOLT:RANG:AUTO ON')
 	keithleyObject.write('SENS:VOLT:NPLC {:.3f}'.format(NPLC))
 	keithleyObject.write('OUTP ON')
@@ -110,7 +111,7 @@ def measureVoltage(keithleyObject, current=0, n=1):
 	data = rawDataArray
 	return data
 
-def prepareCurrent(keithleyObject, NPLC=1, currentlimit=10^-2):
+def prepareCurrent(keithleyObject, NPLC=1, currentlimit=1e-2):
 	'''
 	Prepares the Keithley to measure current.
 	NPLC Range [0.01,10]
@@ -230,21 +231,21 @@ def takeIV(keithleyObject, minV=-0.2, maxV=1.2, stepV=0.1, delay=0.01, forw=1, N
 	data = np.reshape(rawData, (-1,5))
 	return data
 
-def shutdownKeithley(keithleyObject,rm='pyvisa.ResourceManager()'):
+def shutdownKeithley(keithleyObject):
 	if keithleyObject == 'Test':
 		return
 	keithleyObject.write('OUTP OFF')
-	eval(rm).close()
+	rm.close()
 
 if __name__ == "__main__":
 
- 	import matplotlib.pyplot as plt
+	import matplotlib.pyplot as plt
 
 	# rm = pyvisa.ResourceManager()
 	# print(rm.list_resources())
 
-	# keithley = connectToKeithley('GPIB0::22::INSTR')
- 	keithley = connectToKeithley('Test')
+	keithley = connectToKeithley('GPIB0::22::INSTR')
+	# keithley = connectToKeithley('Test')
 	
 # 	openShutter(keithley)
 # 	closeShutter(keithley)
@@ -284,10 +285,10 @@ if __name__ == "__main__":
 # 	plt.plot(xs,dataCurrent[:,1])
 
 
- 	#This section of code tests the takeIV function and plots it.
- 	openShutter(keithley)
- 	# data = takeIV(keithley, NPLC = 0.01)
- 	data = takeIV(keithley,-0.2,1.2,0.02,0.01,0,1) 
+	#This section of code tests the takeIV function and plots it.
+	openShutter(keithley)
+	# data = takeIV(keithley, NPLC = 0.01)
+	data = takeIV(keithley,-0.2,1.2,0.02,0.01,0,1) 
 	# print (type(data))
 	# print (data.shape)
 	# print (type(data[0]))
@@ -297,10 +298,10 @@ if __name__ == "__main__":
 	# print (type(data[0][2]))
 	# print (type(data[0][3]))
 	# print (type(data[0][4]))
- 	volt = data[:,0]
- 	current = data[:,1]
- 	currentdensity=[x*1000/0.06 for x in current]
-     
+	volt = data[:,0]
+	current = data[:,1]
+	currentdensity=[x*1000/0.06 for x in current]
+	 
 	# c = data[:,2]
 	# d = data[:,3]
 	# e = data[:,4]
@@ -322,14 +323,14 @@ if __name__ == "__main__":
 # 	voltDark = dataDark[:,0]
 # 	currentDark = dataDark[:,1]
 # 	plt.figure('Sweep')
- 	# plt.plot(volt,volt, label = 'Volts')
- 	plt.plot(volt,currentdensity, label = 'Jsc->Voc')
+	# plt.plot(volt,volt, label = 'Volts')
+	plt.plot(volt,currentdensity, label = 'Jsc->Voc')
 # 	plt.plot(volt1,current1, label = 'Voc->Jsc')
 	# plt.plot(volt,c, label = 'Resistance') #This is noramlly disabled and will result in all values equal to 9.91e+37
 	# plt.plot(volt,d, label = 'Time')
 	# plt.plot(volt,e, label = 'Status')
 
- 	# plt.plot(voltDark,currentDark, label = 'Dark')
+	# plt.plot(voltDark,currentDark, label = 'Dark')
 	
- 	shutdownKeithley(keithley)
- 	plt.show()
+	shutdownKeithley(keithley)
+	plt.show()
